fee16314a55858cd366caa4412739e6ad3875bc3
Yura Istomin <yura.ist@icloud.com>
2020-06-01 09:58:25 +0300

    layout profile

--- SadAgregator/Sources/Views/Screens/ProfileView.swift
+++ SadAgregator/Sources/Views/Screens/ProfileView.swift
@@ -12,10 +12,13 @@ struct ProfileView: View {
   var body: some View {
     List {
       Section {
-        DetailItemView(mainText: "Имя", detailedText: "Юрий")
-        DetailItemView(mainText: "Телефон", detailedText: "+7 912 557 18 93")
-        DetailItemView(mainText: "Email", detailedText: "yura@mail.ru")
-        DetailItemView(mainText: "Пароль", detailedText: "Изменить")
+        Group {
+          DetailItemView(mainText: "Имя", detailedText: "Юрий")
+          DetailItemView(mainText: "Телефон", detailedText: "+7 912 557 18 93")
+          DetailItemView(mainText: "Email", detailedText: "yura@mail.ru")
+          DetailItemView(mainText: "Пароль", detailedText: "Изменить")
+        }
+        .padding(.vertical, 8)
       }
       
       Section {
@@ -25,15 +28,37 @@ struct ProfileView: View {
             Image("vk_icon")
               .renderingMode(.template)
             Text("Подключить выгрузку")
+              .fontWeight(.medium)
           }
         }
         .buttonStyle(BorderedButtonStyle(color: .blue))
+        .padding(.top)
         
         Text("Подключите Ваш аккаунт ВКонтакте для публикации товаров поставщиков на своей стене")
+          .font(.footnote)
+          .multilineTextAlignment(.center)
+          .foregroundColor(.gray)
+          .frame(maxWidth: .infinity)
+        
+        Button(action: {}) {
+          HStack{
+            Image("ok_icon")
+              .renderingMode(.template)
+            Text("Подключить выгрузку")
+              .fontWeight(.medium)
+          }
+        }
+        .buttonStyle(BorderedButtonStyle(color: .orange))
+        .padding(.top)
         
         Text("Подключите Ваш аккаунт Одноклассников для публикации товаров поставщиков в своей ленте")
+          .font(.footnote)
+          .multilineTextAlignment(.center)
+          .foregroundColor(.gray)
+          .frame(maxWidth: .infinity)
       }
     }
+    .navigationBarTitle("Профиль")
     .onAppear {
       UITableView.appearance().separatorStyle = .none
     }
