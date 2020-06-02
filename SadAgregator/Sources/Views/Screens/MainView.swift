8afbd0790c627fb21f4394d51e182b822e9f4752
Polina <polinakamurko@icloud.com>
2020-06-02 12:48:15 +0300

    show and hide activity

--- SadAgregator/Sources/Views/Screens/MainView.swift
+++ SadAgregator/Sources/Views/Screens/MainView.swift
@@ -16,169 +16,167 @@ struct MainView: View {
   
   var body: some View {
     
-      VStack {
-        if !showCancelButton {
-          Text("Главная")
-            .fontWeight(.semibold)
-            .padding(.top)
-            .transition(.customNavbar)
-        }
-        
-        HStack { // Search Bar
-          HStack {
-            Image(systemName: "magnifyingglass")
-            
-            TextField("Поиск", text: $searchQuery, onEditingChanged: { isEditing in
-              withAnimation {
-                self.showCancelButton = true
-              }
-            }, onCommit: {
-              print("onCommit")
-            })
-              .foregroundColor(.primary)
-            
-            if searchQuery == "" {
-              Button(action: {
-                self.searchQuery = ""
-              }) {
-                Image(systemName: "photo.fill")
-                  .opacity(showCancelButton ? 0 : 1)
-                  .padding(.trailing, 8)
-              }
-            
-            } else {
-              Button(action: {
-                self.searchQuery = ""
-              }) {
-                Image(systemName: "xmark.circle.fill")
-                  .opacity(searchQuery == "" ? 0 : 1)
-              }
-            }
-          }
-          .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
-          .foregroundColor(.secondary)
-          .background(Color(.secondarySystemBackground))
-          .cornerRadius(10.0)
+    VStack {
+      if !showCancelButton {
+        Text("Главная")
+          .fontWeight(.semibold)
+          .padding(.top)
+          .transition(.customNavbar)
+      }
+      
+      HStack { // Search Bar
+        HStack {
+          Image(systemName: "magnifyingglass")
           
-          if showCancelButton {
-            Button("Отмена") {
-              UIApplication.shared.endEditing(true)
+          TextField("Поиск", text: $searchQuery, onEditingChanged: { isEditing in
+            withAnimation {
+              self.showCancelButton = true
+            }
+          }, onCommit: {
+            print("onCommit")
+          })
+            .foregroundColor(.primary)
+          
+          if searchQuery == "" {
+            Button(action: {
               self.searchQuery = ""
-              withAnimation {
-                self.showCancelButton = false
-              }
+            }) {
+              Image(systemName: "photo.fill")
+                .opacity(showCancelButton ? 0 : 1)
+                .padding(.trailing, 8)
             }
-            .foregroundColor(Color(.systemBlue))
+            
+          } else {
+            Button(action: {
+              self.searchQuery = ""
+            }) {
+              Image(systemName: "xmark.circle.fill")
+                .opacity(searchQuery == "" ? 0 : 1)
+            }
           }
         }
-        .padding(.horizontal)
-        .padding(.vertical, 8)
+        .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+        .foregroundColor(.secondary)
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(10.0)
         
-        List {
-          Section {
-            VStack {
-              HStack {
-                Text("Активность")
-                  .font(.system(size: 22, weight: .bold))
-                Spacer()
-                Button(action: {
-                  self.showActivity = true
-                }) {
-                  Image(systemName: "chevron.down")
-                  .font(.system(size: 15, weight: .bold))
-                  .foregroundColor(.blue)
-                }
-                .buttonStyle(BorderlessButtonStyle())
-              }
-              .padding(.horizontal)
-              
-              if showActivity {
-                VStack {
-                  HStack {
-                    Image(systemName: "bolt.fill")
-                    Text("1048 поставщиков")
-                  }
-                  .font(.system(size: 13, weight: .bold))
-                  .foregroundColor(.blue)
-                  
-                  HStack {
-                    VStack(alignment: .leading, spacing: 12) {
-                      Text(" ")
-                      HStack {
-                        Image(systemName: "rectangle.fill.on.rectangle.fill")
-                          .foregroundColor(Color(UIColor.systemGray3))
-                        Text("Посты")
-                          .fixedSize(horizontal: true, vertical: false)
-                      }
-                      HStack {
-                        Image(systemName: "photo.fill.on.rectangle.fill")
-                          .foregroundColor(Color(UIColor.systemGray3))
-                        Text("Фото")
-                      }
-                    }
-                    Spacer()
-                    VStack(spacing: 12) {
-                      Text("Сегодня")
-                        .foregroundColor(.gray)
-                      Text("2253")
-                      Text("10783")
-                    }
-                    .font(.system(size: 17, weight: .bold))
-                    Spacer()
-                    VStack(spacing: 12) {
-                      Text("Вчера")
-                        .foregroundColor(.gray)
-                      Text("2253")
-                      Text("10783")
-                    }
-                  }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background((Color(red: 248/255, green: 248/255, blue: 249/255)))
-              }
-              
-              
-              Group {
-                SectionTitleView("Активность линий", showAllAction: {
-                  // TODO: Add show all action
-                })
-                
-                ActivityItemView(number: 1, title: "Линия 30", subtitle: "17 мин. назад", disclosureText: "1436")
-              }
-              .padding(.horizontal)
+        if showCancelButton {
+          Button("Отмена") {
+            UIApplication.shared.endEditing(true)
+            self.searchQuery = ""
+            withAnimation {
+              self.showCancelButton = false
             }
-            .listRowInsets(EdgeInsets())
-            
-            NavigationLink(destination: LineView()) {
-              ActivityItemView(number: 2, title: "Линия 30", subtitle: "17 мин. назад", disclosureText: "1436")
-            }
-            ActivityItemView(number: 3, title: "Линия 30", subtitle: "17 мин. назад", disclosureText: "1436")
           }
-          
-          Section {
-            VStack {
-              SectionTitleView("Активность точек", showAllAction: {
-                // TODO: Add show all action
-              })
-              ActivityItemView(number: 1, title: "Точка 30", subtitle: "17 мин. назад", disclosureText: "1436")
-            }
-            NavigationLink(destination: SpotView()) {
-              ActivityItemView(number: 2, title: "Точка 30", subtitle: "17 мин. назад", disclosureText: "1436")
-            }
-            ActivityItemView(number: 3, title: "Точка 30", subtitle: "17 мин. назад", disclosureText: "1436")
-          }
-          
-          Section(header: Text("Последние посты")) {
-            PostItemView()
-            PostItemView()
-            PostItemView()
-            PostItemView()
-          }
+          .foregroundColor(Color(.systemBlue))
         }
       }
-      .navigationBarTitle("Главная", displayMode: .inline)
-      .navigationBarHidden(true)
+      .padding(.horizontal)
+      .padding(.vertical, 8)
+      
+      List {
+        Section {
+          VStack {
+            HStack {
+              Text("Активность")
+                .font(.system(size: 22, weight: .bold))
+              Spacer()
+              Button(action: {
+                self.showActivity.toggle()
+              }) {
+                Image(systemName: showActivity ? "chevron.down" : "chevron.right")
+                  .font(.system(size: 15, weight: .bold))
+                  .foregroundColor(.blue)
+              }
+              .buttonStyle(BorderlessButtonStyle())
+            }
+            .padding(.horizontal)
+            
+            VStack {
+              HStack {
+                Image(systemName: "bolt.fill")
+                Text("1048 поставщиков")
+              }
+              .font(.system(size: 13, weight: .bold))
+              .foregroundColor(.blue)
+              if showActivity {
+                HStack {
+                  VStack(alignment: .leading, spacing: 12) {
+                    Text(" ")
+                    HStack {
+                      Image(systemName: "rectangle.fill.on.rectangle.fill")
+                        .foregroundColor(Color(UIColor.systemGray3))
+                      Text("Посты")
+                        .fixedSize(horizontal: true, vertical: false)
+                    }
+                    HStack {
+                      Image(systemName: "photo.fill.on.rectangle.fill")
+                        .foregroundColor(Color(UIColor.systemGray3))
+                      Text("Фото")
+                    }
+                  }
+                  Spacer()
+                  VStack(spacing: 12) {
+                    Text("Сегодня")
+                      .foregroundColor(.gray)
+                    Text("2253")
+                    Text("10783")
+                  }
+                  .font(.system(size: 17, weight: .bold))
+                  Spacer()
+                  VStack(spacing: 12) {
+                    Text("Вчера")
+                      .foregroundColor(.gray)
+                    Text("2253")
+                    Text("10783")
+                  }
+                }
+              }
+            }
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background((Color(red: 248/255, green: 248/255, blue: 249/255)))
+            
+            Group {
+              SectionTitleView("Активность линий", showAllAction: {
+                // TODO: Add show all action
+              })
+              
+              ActivityItemView(number: 1, title: "Линия 30", subtitle: "17 мин. назад", disclosureText: "1436")
+            }
+            .padding(.horizontal)
+          }
+          .listRowInsets(EdgeInsets())
+          
+          NavigationLink(destination: LineView()) {
+            ActivityItemView(number: 2, title: "Линия 30", subtitle: "17 мин. назад", disclosureText: "1436")
+          }
+          ActivityItemView(number: 3, title: "Линия 30", subtitle: "17 мин. назад", disclosureText: "1436")
+        }
+        
+        Section {
+          VStack {
+            SectionTitleView("Активность точек", showAllAction: {
+              // TODO: Add show all action
+            })
+            ActivityItemView(number: 1, title: "Точка 30", subtitle: "17 мин. назад", disclosureText: "1436")
+          }
+          NavigationLink(destination: SpotView()) {
+            ActivityItemView(number: 2, title: "Точка 30", subtitle: "17 мин. назад", disclosureText: "1436")
+          }
+          ActivityItemView(number: 3, title: "Точка 30", subtitle: "17 мин. назад", disclosureText: "1436")
+        }
+        
+        Section(header: Text("Последние посты")) {
+          PostItemView()
+          PostItemView()
+          PostItemView()
+          PostItemView()
+        }
+      }
+    }
+    .navigationBarTitle("Главная", displayMode: .inline)
+    .navigationBarHidden(true)
   }
 }
 
